24c5d3ac4a55d4c477941cf1f13106c1a28a37eb
Frontends <dytpq10@gmail.com>
2024-08-22 16:23:06 +0900

    initial commit

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', reload=True)
