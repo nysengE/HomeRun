974c195205e00071668f38f09163361371c06a58
gongbu22 <pyujin0711@naver.com>
2024-08-22 19:47:16 +0900

    동호회 club 라우터와 동호회 페이지 추가

---
+++ app/routes/club.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+
+club_router = APIRouter()
+templates = Jinja2Templates(directory='views/templates')
+
+@club_router.get('/', response_class=HTMLResponse)
+async def club(req: Request):
+    return templates.TemplateResponse('club/club.html', {'request': req})
