fbc1a0d7703fe9471d6055199dde12e94d688635
gongbu22 <pyujin0711@naver.com>
2024-08-26 00:05:00 +0900

    card 메인페이지에 card, 검색창 html 추가 mypage 회원페이지에 왼쪽 사이드바 html 추가

--- app/routes/mypage.py
+++ app/routes/mypage.py
@@ -1,5 +1,11 @@
 from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 
 mypage_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
+
+@mypage_router.get('/user', response_class=HTMLResponse)
+async def club(req: Request):
+    return templates.TemplateResponse('mypage/user.html', {'request': req})
