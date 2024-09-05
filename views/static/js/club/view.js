f344b1459c6cf1b564fcf93fb6ecefe0c79aa006
gongbu22 <pyujin0711@naver.com>
2024-09-05 13:41:52 +0900

    1차 완료 - 동호회/회원페이지

---
+++ views/static/js/club/view.js
@@ -0,0 +1,118 @@
+// 목록으로 돌아가기
+const prebtn = document.querySelector('#prebtn');
+
+prebtn.addEventListener('click', () => {
+    window.location.href='/club/1';
+});
+
+// 동호회 번호 (clubno) 가져오기
+const clubno = parseInt(document.querySelector('#hiddenclubno').value);
+// console.log('clubno: ', clubno);
+
+
+// 회원 아이디 (userid) 가져오기
+const userid = document.querySelector('#userid').value;
+
+// 신청하기
+const applybtn = document.querySelector('#applybtn');
+if (applybtn) {
+    applybtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        window.location.href = `/club/apply/${clubno}/${userid}`;
+
+        // // post로 보낼 데이터 json 형식으로 만들기
+        // const data = {
+        //     userid: userid,
+        //     clubno: clubno
+        // };
+        //
+        // try {
+        //     await fetch('/club/apply', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //         },
+        //         body: JSON.stringify(data)
+        //     }).then((res) => {
+        //         alert('신청되었습니다.');
+        //         if (res.redirected) {
+        //             location.href = res.url;
+        //         }
+        //     })
+        //
+        // } catch (error) {
+        //     console.error('Error:', error);
+        // }
+
+    });
+};
+
+// 댓글창
+let replyfrm = document.replyfrm;
+
+replyfrm?.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(replyfrm);
+
+    let jsondata = {};
+    formData.forEach((val, key) => {
+        jsondata[key] = val;
+    });
+
+    // formData.forEach((val, key) => {
+    //     console.log(key, val);
+    // });
+
+    fetch('/club/reply', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsondata),
+        redirect: 'follow'
+    }).then((res) => {
+        if (res.redirected) location.href = res.url;
+    });
+});
+
+// '추가'버튼 누르면 hidden 댓글번호(rpno) value 값 넣기
+const addrreply = (rno) => {
+    let rpno = document.querySelector('#rpno');
+    rpno.value = rno;
+};
+
+// 대댓글창 등록
+const rreplyfrm = document.rreplyfrm;
+
+rreplyfrm.addEventListener('submit', (e, rpno) => {
+    e.preventDefault();
+
+    const formData = new FormData(rreplyfrm);
+
+    let jsondata = {};
+    formData.forEach((val, key) => {
+        jsondata[key] = val;
+    });
+
+
+    // formData.forEach((val, key) => {
+    //     console.log(key, val);
+    // });
+
+    fetch('/club/rreply', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsondata),
+        redirect: 'follow'
+    }).then((res) => {
+        if (res.redirected) location.href = res.url;
+    });
+});
+
+
---
+++ views/static/js/mypage/clubwrite.js
@@ -0,0 +1,139 @@
+const applyusers = document.querySelectorAll('.applyusers');
+
+applyusers.forEach((applybtn) => {
+    applybtn.addEventListener('click', async (e) => {
+        const clubno = applybtn.querySelector('#applyclubno').value;
+
+        try {
+           let jsondata = {};
+           jsondata['clubno'] = clubno;
+
+           fetch(`/mypage/clubwrite/apply`, {
+            method: 'POST',
+               headers: {
+                   'Accept': 'application/json',
+                   'Content-Type': 'application/json'
+               },
+               body: JSON.stringify(jsondata),
+               redirect: 'follow'
+           }).then((res) => {
+               // if (res.redirected) location.href = res.url;
+               return res.json();
+           })
+           .then((data) => {
+               // console.log(data);
+               if (data.applylist) {
+                   // console.log('Apply List:', data.applylist);
+                   // 추가적인 처리 로직을 여기에 추가
+                   applylist = data.applylist;
+
+                   const tableBody = document.getElementById('applyTableBody');
+                   const table = document.getElementById('applyTable');
+                   const noApplicants = document.getElementById('noApplicants');
+
+                   tableBody.innerHTML = '';
+
+                   if (applylist && applylist.length > 0) {
+                       // 신청자 목록이 있을 때 테이블 표시
+                       applylist.forEach(applicant => {
+                           const row = document.createElement('tr');
+                           // console.log('applicant: ', applicant);
+
+                           // 버튼의 활성화 상태를 결정하는 조건
+                           const buttonClass = applicant.status === '승인' ? 'btn-secondary' : 'btn-success';
+                           const buttonDisabled = applicant.status === '승인' ? 'disabled' : '';
+
+                           row.innerHTML = `
+                            <td>${applicant.userid}</td>
+                            <td>${applicant.regdate.strftime('%Y-%m-%d')}</td>
+                            <td>${applicant.status}</td>
+                            <td>
+                                <button type="button" class="btn ${buttonClass}" ${buttonDisabled} data-ano="${applicant.ano}">승인</button>
+                            </td>
+                        `;
+                           tableBody.appendChild(row);
+                       });
+                       table.style.display = 'table';
+                       noApplicants.style.display = 'none';
+                   } else {
+                       // 신청자 목록이 없을 때 메시지 표시
+                       table.style.display = 'none';
+                       noApplicants.style.display = 'block';
+                   }
+
+                   // 승인 버튼에 이벤트 추가
+                   const approveButtons = document.querySelectorAll('.btn-success:not([disabled])');
+                   approveButtons.forEach(button => {
+                       button.addEventListener('click', async (e) => {
+                           const ano = button.getAttribute('data-ano');
+
+                           console.log('ano: ', ano);
+
+                           try {
+                               const response = await fetch('/mypage/clubwrite/approve', {
+                                   method: 'POST',
+                                   headers: {
+                                       'Accept': 'application/json',
+                                       'Content-Type': 'application/json'
+                                   },
+                                   body: JSON.stringify({ ano })
+                               });
+
+                               const result = await response.json();
+                               if (result.success) {
+                                   alert('승인되었습니다.');
+                                   button.classList.replace('btn-success', 'btn-secondary');
+                                   button.setAttribute('disabled', true);
+
+                                   window.location.href = '/mypage/clubwrite';
+                               } else {
+                                   alert('승인 실패.');
+                               }
+                           } catch (error) {
+                               console.error('승인 중 오류 발생:', error);
+                           }
+                       });
+                   });
+
+               }
+            })
+
+
+        } catch(error) {
+            console.error('데이터 가져오기 실패:', error);
+        }
+
+    })
+});
+
+// 수정 페이지로 이동
+const modifyclub = (clubno) => {
+    console.log('clubno: ', clubno);
+    window.location.href = '/mypage/clubwrite/modify/'+clubno;
+}
+
+// 삭제
+const deleteclub = (clubno) => {
+    if (confirm('정말 삭제하시겠습니까?')) {
+        fetch(`/mypage/clubwrite/${clubno}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if(data.success){
+                    alert('삭제되었습니다.');
+                    location.reload();
+                } else {
+                    alert('삭제되지않았습니다.')
+                }
+        })
+            .catch(error => {
+                console.error('삭제 중 오류 발생: ', error);
+                alert('오류가 발생했습니다.');
+            })
+    }
+};
+
